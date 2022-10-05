ca2813bed2b5ba9d0fa0626dfcb8ff51d7145195
Lauris <plume.lauris@gmail.com>
2022-10-05 17:04:53 +0300

    make it better

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Starting server at ${port}`);
+//const port = process.env.PORT || 3000;
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Starting server at ${process.env.PORT}`);
 });
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
